b0118a8d80e03f1489f1403ae2fb486e341f8d45
Karthik Bommanal <karthikb1105@gmail.com>
2025-06-03 02:11:06 +0530

    Updated the codes

--- Backend/Config/database.js
+++ Backend/Config/database.js
@@ -1,12 +1,28 @@
+// require('dotenv').config();
+// const mongoose = require('mongoose');
+
+// let connectDb = async () => {
+//     try {
+//         await mongoose.connect(process.env.MONGO_URL, {
+//         //   useNewUrlParser: true,
+//         //   useUnifiedTopology: true,
+//         });
+//         console.log("The DB is connected");
+//     } catch (err) {
+//         console.error("Error connecting to the database:", err.message);
+//     }
+// };
+
+// module.exports = connectDb;
+
+
+
 require('dotenv').config();
 const mongoose = require('mongoose');
 
 let connectDb = async () => {
     try {
-        await mongoose.connect(process.env.MONGO_URL, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        });
+        await mongoose.connect(process.env.MONGO_URL);
         console.log("The DB is connected");
     } catch (err) {
         console.error("Error connecting to the database:", err.message);
