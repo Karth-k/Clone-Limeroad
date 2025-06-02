87a20a6ab3545c49f3964847581a733b10394f74
Karthik Bommanal <karthikb1105@gmail.com>
2025-06-03 02:05:40 +0530

    Updated the code

--- Backend/Config/database.js
+++ Backend/Config/database.js
@@ -1,8 +1,12 @@
+require('dotenv').config();
 const mongoose = require('mongoose');
 
 let connectDb = async () => {
     try {
-        await mongoose.connect(process.env.mongo_url);
+        await mongoose.connect(process.env.MONGO_URL, {
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
+        });
         console.log("The DB is connected");
     } catch (err) {
         console.error("Error connecting to the database:", err.message);
